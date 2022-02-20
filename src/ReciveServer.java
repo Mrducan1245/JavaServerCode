233eb777ce8416a1529d40c63c4b457228e24707
Mrducan1245 <mrducan12345@163.com>
2022-02-20 22:17:31 +0800

    服务端接收图片完成版

--- src/ReciveServer.java
+++ src/ReciveServer.java
@@ -1,8 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
-
-import static java.lang.System.in;
 
 public class ReciveServer<fileURL> {
     private static final String filePath = "C:\\Users\\Mrducan\\Desktop\\picture\\";
@@ -12,36 +9,68 @@ public class ReciveServer<fileURL> {
 //        reciveServer.fileURL = reciveServer.getFileName();
         ServerSocket serverSocket = new ServerSocket(8009);
         System.out.println("监听中...");
-        System.out.println("文件地址及名字："+fileURL);
+
         while (true){
-            fileURL = filePath + getFileName();
-            ioStreamSet(serverSocket);
+            try {
+                Socket socket1 =serverSocket.accept() ;
+                String[] mFileNameHostIP = getFileNameHostIP(socket1);
+                socket1.close();
+
+                fileURL = filePath + mFileNameHostIP[1]+"\\"+mFileNameHostIP[0];
+                File dir = new File(filePath + mFileNameHostIP[1]);
+                if (!dir.exists()) {// 判断目录是否存在
+                    dir.mkdir();
+                }
+                System.out.println("文件地址是："+fileURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Socket socket2 = serverSocket.accept();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                        ioStreamSet(socket2);
+                    try {
+                        socket2.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
     }
 
-    public static String getFileName(){
-        DatagramSocket ds = null;
+
+
+    public static String[] getFileNameHostIP(Socket socket){
+
+        String[] fileNameHostIP = null;
         String fileName = null;
+        String cilentHostIP = null;
         try {
-            ds=new DatagramSocket(9000);
-            byte[] data = new byte[1024];
-            DatagramPacket dp=new DatagramPacket(data,data.length);
-            ds.receive(dp);
-            fileName = new String(data, 0, dp.getLength());
+
+            BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            cilentHostIP = bufferedReader.readLine();
+            fileName = bufferedReader.readLine();
+
+            fileNameHostIP = new String[2];
+            fileNameHostIP[0] = fileName;
+            fileNameHostIP[1] = cilentHostIP;
             System.out.println("文件名为："+fileName);
+            System.out.println("客户端地址是："+cilentHostIP);
+
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            ds.close();
         }
-        return  fileName;
+        return fileNameHostIP;
+
     }
 
 
-    public static void ioStreamSet(ServerSocket serverSocket){
-        Socket socket = null;
+
+    public static void ioStreamSet(Socket socket){
         try {
-            socket = serverSocket.accept();
             InputStream in = socket.getInputStream();
             DataInputStream dis = new DataInputStream(in);
             FileOutputStream fOS = new FileOutputStream(fileURL);
@@ -54,12 +83,12 @@ public class ReciveServer<fileURL> {
             fOS.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+//        }finally {
+//            try {
+//                socket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
